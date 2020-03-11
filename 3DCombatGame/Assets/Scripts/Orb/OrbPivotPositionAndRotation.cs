ea46efc662efa14c02fc993dd642426c19d6e31c
arturo101199 <al375559@uji.es>
2020-03-11 12:11:12 +0100

    Orb colliding with walls, almost done

--- 3DCombatGame/Assets/Scripts/Orb/OrbPivotPositionAndRotation.cs
+++ 3DCombatGame/Assets/Scripts/Orb/OrbPivotPositionAndRotation.cs
@@ -6,14 +6,12 @@ public class OrbPivotPositionAndRotation : MonoBehaviour
 {
     public TransformValue targetPositionTransform;
     public TransformValue targetRotationTransform;
-    public float rotationSpeed = 2;
     
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = targetPositionTransform.GetValue().position;
 
-        //transform.rotation = Quaternion.Slerp(transform.rotation, targetPositionTransform.value.rotation, rotationSpeed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(0f, targetRotationTransform.GetValue().rotation.eulerAngles.y, 0f);
 
     }
--- 3DCombatGame/Assets/Scripts/Orb/OrbPosition.cs
+++ 3DCombatGame/Assets/Scripts/Orb/OrbPosition.cs
@@ -5,41 +5,100 @@ using UnityEngine;
 public class OrbPosition : MonoBehaviour
 {
     public Vector3 desiredPosition;
-    
+    public Transform targetTransform;
+    public LayerMask collisionLayer;
+
+    //Oscilation parameters
+    #region
+
     float maxY = 0.05f;
     float minY = -0.05f;
     float velY = 0.0f;
     float smoothTime = 1f;
     bool up;
-    // Start is called before the first frame update
+
+    #endregion
+
+    bool colliding = false;
+    Vector3 direction;
+    float orbDistance;
+    float offsetMultiplier = 1.2f;
+    float radiusOffset = 0.2f;
+
     void Start()
     {
-        
+        direction = (transform.position - targetTransform.position).normalized;
+        orbDistance = Vector3.Distance(transform.position, targetTransform.position);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if(Mathf.Abs(transform.localPosition.x - desiredPosition.x) > 0.05f || Mathf.Abs(transform.localPosition.z - desiredPosition.z) > 0.05f)
+        transform.LookAt(targetTransform.position);
+        Debug.DrawRay(targetTransform.position, (transform.position - targetTransform.position) * offsetMultiplier, Color.red);
+        Oscillate();
+    }
+
+    void FixedUpdate()
+    {
+        CheckColliding(targetTransform.position);
+        if (colliding)
         {
-            transform.localPosition = Vector3.Slerp(transform.localPosition, desiredPosition, Time.deltaTime * 3f);
+            float distance = Mathf.Clamp(GetAdjustedDistanceWithRayFrom(targetTransform.position), radiusOffset + 0.05f, Mathf.Infinity);
+            transform.localPosition = direction * (distance - radiusOffset);
         }
         else
         {
-            if (up)
-            {
-                if (maxY - transform.localPosition.y > 0.01f)
-                    transform.localPosition = new Vector3(transform.localPosition.x, Mathf.SmoothDamp(transform.localPosition.y, maxY, ref velY, smoothTime), transform.localPosition.z);
-                else up = false;
-            }
-            else
-            {
-                if ( transform.localPosition.y - minY > 0.01f)
-                {
-                    transform.localPosition = new Vector3(transform.localPosition.x, Mathf.SmoothDamp(transform.localPosition.y, minY, ref velY, smoothTime), transform.localPosition.z);
-                }
-                else up = true;
-            }
+            transform.localPosition = desiredPosition;
         }
+    }
+
+    void Oscillate()
+    {
+        if (up)
+        {
+            if (maxY - transform.localPosition.y > 0.01f)
+                transform.localPosition = new Vector3(transform.localPosition.x, Mathf.SmoothDamp(transform.localPosition.y, maxY, ref velY, smoothTime), transform.localPosition.z);
+            else up = false;
+        }
+        else
+        {
+            if (transform.localPosition.y - minY > 0.01f)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, Mathf.SmoothDamp(transform.localPosition.y, minY, ref velY, smoothTime), transform.localPosition.z);
+            }
+            else up = true;
+        }
+    }
+
+
+    bool CollisionDetected(Vector3 position, Vector3 targetPos)
+    {
+        Ray ray = new Ray(targetPos, (position - targetPos).normalized);
+        Debug.DrawRay(targetPos, (position - targetPos).normalized * orbDistance * offsetMultiplier, Color.green);
+        //float distance = Vector3.Distance(position, targetPos);
+        if(Physics.Raycast(ray, orbDistance * offsetMultiplier, collisionLayer))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    float GetAdjustedDistanceWithRayFrom(Vector3 from)
+    {
+        float distance = -1;
+        Ray ray = new Ray(from, transform.position - from);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            distance = hit.distance;
+        }
+
+        if (distance == -1)
+            return 0;
+        else return distance;
+    }
+
+    void CheckColliding(Vector3 targetPos)
+    {
+        colliding = (CollisionDetected(transform.position, targetPos));
     }
 }
