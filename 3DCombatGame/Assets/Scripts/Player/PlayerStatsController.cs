f055a96d00c60bedd3c168f5b428ba312c278070
Dominik Konrad Kwiek <randomgameplayer1234@gmail.com>
2020-05-06 16:35:59 +0200

    UIBasica

---
+++ 3DCombatGame/Assets/Scripts/Player/PlayerStatsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatsController : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Slider staminaSlider;
+
+    public FloatValue stamina;
+
+    public void SetMaxHealth(float health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        healthSlider.value = health;
+    }
+
+    public void takeDamage(float damage)
+    {
+        SetHealth(healthSlider.value - damage);
+    }
+
+    public void SetMaxStamina(float stamina)
+    {
+        staminaSlider.maxValue = stamina;
+        staminaSlider.value = stamina;
+    }
+
+    public void SetStamina(float stamina)
+    {
+        staminaSlider.value = stamina;
+    }
+
+    private void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        Stamina staminaManager = player.GetComponent<Stamina>();
+        SetMaxStamina(staminaManager.maxStamina);
+    }
+
+    private void Update()
+    {
+        SetStamina(stamina.GetValue());
+    }
+}
