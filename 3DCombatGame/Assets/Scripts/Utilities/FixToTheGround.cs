3949a669a747831a82395f268eab4213374f0173
arturo101199 <al375559@uji.es>
2020-05-08 18:53:49 +0200

    FixToTheGround Done

---
+++ 3DCombatGame/Assets/Scripts/Utilities/FixToTheGround.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixToTheGround : MonoBehaviour
+{
+
+    void Start()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, 10);
+        transform.position = hit.point;
+    }
+
+}
